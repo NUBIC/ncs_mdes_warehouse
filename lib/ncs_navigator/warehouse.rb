e225595fdd46b4b5e28f6ad3f0c3ed93adb0b2b2
Rhett Sutphin <rhett@detailedbalance.net>
2011-08-02 10:26:02 -0500

    Introduce NCS_NAVIGATOR_ENV & an accessor. Determine the bcdatabase group to use from this variable.

--- lib/ncs_navigator/warehouse.rb
+++ lib/ncs_navigator/warehouse.rb
@@ -9,6 +9,30 @@ module NcsNavigator
 
     class << self
       ##
+      # The name of the environment in which this warehouse is
+      # running; ex: "development" (The default), "staging", or
+      # "production". This value can be set from the environment
+      # variable `NCS_NAVIGATOR_ENV`.
+      #
+      # @return [String]
+      def env
+        @env ||= ENV['NCS_NAVIGATOR_ENV'] || 'development'
+      end
+
+      ##
+      # Change the warehouse's runtime environment. Using the
+      # environment variable `NCS_NAVIGATOR_ENV` is preferable because
+      # it will ensure that the environment is set to the desired
+      # value from the beginning of the process. This method is mainly
+      # for use in tests.
+      #
+      # @param [String] name
+      # @return [String]
+      def env=(name)
+        @env = name
+      end
+
+      ##
       # @return [Bcdatabase::DatabaseConfigurations] the set of
       #   bcdatabase configurations to use throughout the warehouse.
       def bcdatabase
@@ -22,6 +46,25 @@ module NcsNavigator
       # @param [Bcdatabase::Configurations] bcdb
       def bcdatabase=(bcdb)
         @bcdatabase = bcdb
+      end
+
+      ##
+      # The bcdatabase group the warehouse will use to find its own
+      # databases and which is used by default in {database
+      # transformers Transformers::Database}.
+      #
+      # @return [Symbol]
+      def default_bcdatabase_group
+        case env
+        when 'development'
+          :local_postgresql
+        when 'staging'
+          :ncsdb_staging
+        when 'production'
+          :ncsdb_prod
+        else
+          raise "Unknown environment #{env}. Please set the bcdatabase group yourself."
+        end
       end
     end
   end
--- spec/ncs_navigator/warehouse_spec.rb
+++ spec/ncs_navigator/warehouse_spec.rb
@@ -3,19 +3,64 @@ require File.expand_path('../../spec_helper', __FILE__)
 module NcsNavigator
   describe Warehouse do
     before do
-      # preserve the value set for other tests
-      @original = Warehouse.bcdatabase
-      Warehouse.bcdatabase = nil
+      # clear for testing
+      @original_env, Warehouse.env = Warehouse.env, nil
     end
 
     after do
-      Warehouse.bcdatabase = @original
+      Warehouse.env = @original_env
+    end
+
+    describe '.env' do
+      it 'defaults to "development"' do
+        Warehouse.env.should == 'development'
+      end
+
+      it 'can be specified in the runtime environment' do
+        ENV['NCS_NAVIGATOR_ENV'] = 'staging'
+        Warehouse.env.should == 'staging'
+      end
+
+      it 'can be set explicitly' do
+        Warehouse.env = 'prod'
+        Warehouse.env.should == 'prod'
+      end
     end
 
     describe '.bcdatabase' do
+      before do
+        # preserve the value set for other tests
+        @original = Warehouse.bcdatabase
+        Warehouse.bcdatabase = nil
+      end
+
+      after do
+        Warehouse.bcdatabase = @original
+      end
+
       it 'has a default value' do
         Warehouse.bcdatabase = nil
         Warehouse.bcdatabase.should be_a Bcdatabase::DatabaseConfigurations
+      end
+    end
+
+    describe '.default_bcdatabase_group' do
+      subject { Warehouse.default_bcdatabase_group }
+
+      [
+        %w(local_postgresql development),
+        %w(ncsdb_staging staging),
+        %w(ncsdb_prod production),
+      ].collect { |exp, env| [exp.to_sym, env] }.each do |expected, environment|
+        it "is #{expected} for #{environment}" do
+          Warehouse.env = environment
+          subject.should == expected
+        end
+      end
+
+      it 'throws an exception for an unknown environment' do
+        Warehouse.env = 'the_moon'
+        lambda { subject }.should raise_error(/unknown environment the_moon/i)
       end
     end
   end
