7f9155872af184e9faba624fcbcaa0a4be8396b6
Rhett Sutphin <rhett@detailedbalance.net>
2011-09-12 14:12:17 -0500

    Make MDES version a command-line configurable option.

--- lib/ncs_navigator/warehouse/cli.rb
+++ lib/ncs_navigator/warehouse/cli.rb
@@ -4,10 +4,18 @@ require 'thor'
 
 module NcsNavigator::Warehouse
   class CLI < Thor
+    class_option 'mdes-version', :type => :string, :default => '2.0',
+      :desc => 'The MDES version for the warehouse in use', :banner => 'X.Y'
+
+    no_tasks {
+      def use_mdes
+        NcsNavigator::Warehouse.use_mdes_version(options[:'mdes-version'])
+      end
+    }
+
     desc 'create-schema', '(Re)builds the schema in the working database'
     def create_schema
-      # TODO: this should be configurable somehow
-      NcsNavigator::Warehouse.use_mdes_version('2.0')
+      use_mdes
 
       db = DatabaseInitializer.new
       db.set_up_repository(:working)
